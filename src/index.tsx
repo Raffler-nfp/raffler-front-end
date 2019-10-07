78ab82c042125811d91bf158e6c0790fea754e55
Sonam Chhogyel <schyles@gmail.com>
2019-10-07 15:48:08 +1000

    add style provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,20 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
 import App from "./App";
 import theme from "./theme";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "raffler",
+  disableGlobal: true
+});
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <StylesProvider generateClassName={generateClassName}>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StylesProvider>,
   document.querySelector("#root")
 );
